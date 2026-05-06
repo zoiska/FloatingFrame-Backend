0b53a47570fd66b35d8c28275938ffad7e7b755b
Alik <alik.wansiedler@stud.hshl.de>
2026-05-06 20:29:50 +0200

    initial backend setup (express, pg, dotenv, cors, nodemon)

---
+++ src/db/db.js
@@ -0,0 +1,21 @@
+// Load .env
+require("dotenv").config();
+
+console.log("DB Config used:");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
+// Import pool for postgres
+const { Pool } = require("pg");
+
+// Create pool for multiple connections with credentials
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pool;
