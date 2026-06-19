dd5cdd18c53ce9b9326ab4ce5de533e6466d4649
Zois Karagiannis <zoisk64@gmail.com>
2026-06-19 17:06:32 -0400

    install fs, https, serve over https

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+import fs from "fs";
+import https from "https";
+
 // Starting point for our Server
 const app = require("./src/app");
 
+const options = {
+  key: fs.readFileSync("./certs/key.pem"),
+  cert: fs.readFileSync("./certs/cert.pem"),
+};
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server läuft auf Port ${PORT}`);
+https.createServer(options, app).listen(PORT, () => {
+  console.log("HTTPS server läuft");
 });
