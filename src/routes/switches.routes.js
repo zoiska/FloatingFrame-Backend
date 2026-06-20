c13eb8b6e8fac99bb2a1130977e3aa626970b96e
Alik <alik.wansiedler@stud.hshl.de>
2026-06-20 18:48:53 +0200

    added CRUD-Operations for switches

--- src/routes/switches.routes.js
+++ src/routes/switches.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const controller = require("../controllers/switches.controllers");
 
-// Endpoint for "/api/switch"
-router.get("/", controller.getAllSwitches);
-
-// Endpoint for "/api/switch/<id>"
-router.get("/:id", controller.getSingleSwitches);
+// Endpoint for /api/switch/...
+router.get("/", controller.getAllSwitches); // Get all Switches
+router.get("/:id", controller.getSwitches); // Get switch by id
+router.post("/", controller.createSwitches); // Create switch
+router.patch("/:id", controller.updateSwitches); // Update switch by id
+router.delete("/:id", controller.deleteSwitches); // Delete switch by id
 
 module.exports = router;
